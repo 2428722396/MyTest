c0adb4161e75d13cbece5de35ed03d475e658e53
lanouhn <2428722396@qq.com>
2016-05-26 17:28:54 +0800

    aaaaaaaaaa

---
+++ Test/Test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 王国旗. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"df");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Test/Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 王国旗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
